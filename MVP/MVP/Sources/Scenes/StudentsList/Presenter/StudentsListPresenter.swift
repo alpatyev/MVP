76dfe7e033c646ac7baf604435dabef7437038e5
AlpatyevNikita <chococacaococonut@gmail.com>
2023-01-29 22:15:25 +0300

    added view for second screen

--- MVP/MVP/Sources/Scenes/StudentsList/Presenter/StudentsListPresenter.swift
+++ MVP/MVP/Sources/Scenes/StudentsList/Presenter/StudentsListPresenter.swift
@@ -5,21 +5,34 @@
 //  Created by Nikita Alpatiev on 1/25/23.
 //
 
+import Foundation
+
 // MARK: - Presenter protocol
 
 protocol StudentsListPresenterProtocol: AnyObject {
     var view: StudentsListViewProtocol? { get set }
+    func updateViewData()
     func userTappedItself()
+    func selectedRow(at path: IndexPath)
 }
 
 // MARK: - Students list presenter class
 
 final class StudentsListPresenter: StudentsListPresenterProtocol {
-    
+
     // MARK: - Models
     
-    private var model: StudentsGroupModel?
-    private var student: Student?
+    private var model: StudentsGroupModel? {
+        willSet {
+            view?.updateSubviews(with: newValue)
+        }
+    }
+    
+    private var student: Student? {
+        willSet {
+            view?.updateSubviews(with: newValue)
+        }
+    }
     
     // MARK: - View
     
@@ -27,17 +40,28 @@ final class StudentsListPresenter: StudentsListPresenterProtocol {
     
     // MARK: - Configure with model and view
     
-    public func configure(with model: StudentsGroupModel?, _ student: Student?,_ view: StudentsListViewProtocol?) {
+    public func configure(with model: StudentsGroupModel?, student: Student?, view: StudentsListViewProtocol?) {
         self.model = model
         self.student = student
         self.view = view
     }
-
+    
+    // MARK: - Update view
+    
+    func updateViewData() {
+        view?.updateSubviews(with: model)
+        view?.updateSubviews(with: student)
+    }
     
     // MARK: - Move to detail scene
     
-     func userTappedItself() {
-         let detailView = SceneBuilder.createDetailScene(with: student)
+    func userTappedItself() {
+        let detailView = SceneBuilder.createDetailScene(with: student)
+        view?.performViewController(detailView)
+    }
+    
+    func selectedRow(at path: IndexPath) {
+        let detailView = SceneBuilder.createDetailScene(with: model?.findPerson(at: path))
         view?.performViewController(detailView)
     }
 }
---
+++ MVP/MVP/Sources/Scenes/StudentsList/View/ScoreLabel.swift
@@ -0,0 +1,18 @@
+//
+//  ScoreLabel.swift
+//  MVP
+//
+//  Created by Nikita Alpatiev on 1/29/23.
+//
+
+import UIKit
+
+class ScoreLabel: UILabel {
+    
+    
+
+    override func layoutSubviews() {
+        super.la
+    }
+
+}
